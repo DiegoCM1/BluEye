e49238ecb2e93d9f01d33a0b3ed800edc40cd177
Diego Colín <luiscolin764@gmail.com>
2025-07-26 23:06:28 -0600

    Corrected typo

--- backend/src/routes/alerts.js
+++ backend/src/routes/alerts.js
@@ -1,7 +1,7 @@
 // backend/src/routes/alerts.routes.js
 
 import { Router } from 'express';
-import { listAlerts, getAlert } from '../controllers/alerts.controller.js';
+import { listAlerts, getAlert } from '../controllers/alertsController.js';
 
 const router = Router();
 
